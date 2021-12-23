54d71c17e6860d721b70e265b9e091b997a68b10
joedoe <d_joe@gmx.net>
2021-12-23 23:25:57 +0100

    structure: chapters added

--- app/src/main/java/net/joedoe/app/App.java
+++ app/src/main/java/net/joedoe/app/App.java
@@ -1,23 +1,7 @@
 package net.joedoe.app;
 
-import net.joedoe.builder.NutritionFacts;
-import net.joedoe.builder.pizza.*;
-
 public class App {
     public static void main(String[] args) {
-        App.item2builder();
-    }
-
-    private static void item2builder() {
-        NutritionFacts cocaCola = new NutritionFacts.Builder(240, 8)
-                .calories(100).sodium(35).carbohydrate(27).build();
-        System.out.println(cocaCola);
-
-        NyPizza pizza = new NyPizza.Builder(NyPizza.Size.SMALL)
-                .addTopping(Pizza.Topping.SAUSAGE).addTopping(Pizza.Topping.ONION).build();
-        System.out.println(pizza);
-        Calzone calzone = new Calzone.Builder()
-                .addTopping(Pizza.Topping.HAM).sauceInside().build();
-        System.out.println(calzone);
+        Chapter2.item2builder();
     }
 }
---
+++ app/src/main/java/net/joedoe/app/Chapter2.java
@@ -0,0 +1,26 @@
+package net.joedoe.app;
+
+import net.joedoe.builder.NutritionFacts;
+import net.joedoe.builder.pizza.Calzone;
+import net.joedoe.builder.pizza.NyPizza;
+import net.joedoe.builder.pizza.Pizza;
+
+/**
+ * <strong>Title</strong>: Creating and Destroying Objets
+ * <br>
+ * <strong>Items</strong>: 1-9
+ */
+public class Chapter2 {
+    protected static void item2builder() {
+        NutritionFacts cocaCola = new NutritionFacts.Builder(240, 8)
+                .calories(100).sodium(35).carbohydrate(27).build();
+        System.out.println(cocaCola);
+
+        Pizza nyPizza = new NyPizza.Builder(NyPizza.Size.SMALL)
+                .addTopping(Pizza.Topping.SAUSAGE).addTopping(Pizza.Topping.ONION).build();
+        System.out.println(nyPizza);
+        Pizza calzone = new Calzone.Builder()
+                .addTopping(Pizza.Topping.HAM).sauceInside().build();
+        System.out.println(calzone);
+    }
+}
